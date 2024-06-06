d1bda2f10df16223f691fb0ac9cff580d56a0bdf
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-06-06 15:16:42 +0530

    googleAuth working

--- src/screens/LoginPage/LoginPage.js
+++ src/screens/LoginPage/LoginPage.js
@@ -1,9 +1,33 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image, Alert } from 'react-native'
+import React, { useEffect } from 'react'
 import auth from '@react-native-firebase/auth';
 import { styles } from './Style';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const LoginPage = () => {
+
+    useEffect(()=>{
+        GoogleSignin.configure({
+            webClientId: '119563383988-079qasngsvf6rm3avcl5ceg0rerpb8a6.apps.googleusercontent.com',
+          });
+    },[])
+
+    async function onGoogleButtonPress() {
+      try {
+          // Check if your device supports Google Play
+          await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+          // Get the users ID token
+          const { idToken } = await GoogleSignin.signIn();
+            console.log(idToken);
+          // Create a Google credential with the token
+          const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+        
+          // Sign-in the user with the credential
+          return auth().signInWithCredential(googleCredential);
+      } catch (error) {
+        Alert.alert(error)
+      }
+      }
 
     const handleanonymous = () => {
         auth()
@@ -25,7 +49,7 @@ const LoginPage = () => {
                 <Text style={[styles.underline,{alignSelf:'center',marginVertical:30}]}>Guest</Text>
             </TouchableOpacity>
            <View style={{flexDirection:'row',gap:40,justifyContent:'center'}}>
-                <TouchableOpacity style={{width:50,justifyContent:'center',alignItems:'center'}}>
+                <TouchableOpacity onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))} style={{width:50,justifyContent:'center',alignItems:'center'}}>
                     <Image
                     style={styles.googleLogo}
                     source={require('../../Assets/images/google-icon.png')}
