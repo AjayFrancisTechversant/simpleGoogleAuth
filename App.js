e50d87c1401518f79da0b95550283f149122cced
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-06-10 15:04:18 +0530

    deeplink done

--- App.js
+++ App.js
@@ -4,36 +4,77 @@ import LoginPage from './src/screens/LoginPage/LoginPage'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import auth from '@react-native-firebase/auth';
+import { PermissionsAndroid } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import Screen1 from './src/screens/Screen1/Screen1'
+import Screen2 from './src/screens/Screen2/Screen2'
 
+// PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
 const Stack = createNativeStackNavigator()
 
 const App = () => {
+  useEffect(()=>{
+    // getFCM()
+  },[])
+
+  const linking = {
+    prefixes: ['myapp://', 'https://myapp.com'],
+    config: {
+     screens:{
+      HomeScreen:{
+        path:'HomeScreen'
+      },
+      Screen1:{
+        path:'Screen1'
+      },
+      Screen2:{
+        path:'Screen2'
+      }
+     }
+    },
+  };
+  
+
+  // const getFCM=async()=>{
+  //   try {
+  //     const fcmToken=await messaging().getToken()
+  //     console.log(fcmToken);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
 
-   // Set an initializing state whilst Firebase connects
-   const [initializing, setInitializing] = useState(true);
-   const [user, setUser] = useState();
- 
-   // Handle user state changes
-   function onAuthStateChanged(user) {
-     setUser(user);
-     if (initializing) setInitializing(false);
-   }
- 
-   useEffect(() => {
-     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-     return subscriber; // unsubscribe on unmount
-   }, []);
- 
-   if (initializing) return null;
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <Stack.Navigator>
         {!user ?
           <Stack.Screen name='LoginPage' component={LoginPage} />
           :
-          <Stack.Screen name='HomeScreen' component={HomeScreen} />
+         <>
+            <Stack.Screen name='HomeScreen' component={HomeScreen} />
+            <Stack.Screen name='Screen1' component={Screen1} />
+            <Stack.Screen name='Screen2' component={Screen2} />
+
+         </>
+
         }
       </Stack.Navigator>
 
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,23 +1,72 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import auth from '@react-native-firebase/auth';
+import notifee from '@notifee/react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid} from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
 
-    const handleLogout = () => {
-        auth()
-            .signOut()
-            .then(() => console.log('User signed out!'));
-    }
-    return (
-        <View>
-            <Text>WELCOME</Text>
+  useEffect(()=>{
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+  })
 
-            <TouchableOpacity onPress={handleLogout}>
-                <Text>logout</Text>
-            </TouchableOpacity>
-        </View>
-    )
+  const displayNotification = async () => {
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: '222',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        //   smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: '222',
+        },
+      },
+    })
+  }
+
+  const handleGetFCMToken=async()=>{
+    const FCMToken= await messaging().getToken()
+    console.log('FCMToken:',FCMToken);
+  }
+
+
+  const handleLogout = () => {
+    GoogleSignin.signOut().then(() => console.log('google logout'))
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  }
+  
+  return (
+    <View>
+      <Text>WELCOME</Text>
+      <TouchableOpacity style={{borderRadius:5,backgroundColor:'gray',height:30,width:100,margin:10,justifyContent:'center',alignItems:'center'}} onPress={displayNotification}>
+        <Text style={{color:'white'}}>notification</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity style={{borderRadius:5,backgroundColor:'gray',height:30,width:100,margin:10,justifyContent:'center',alignItems:'center'}}  onPress={handleGetFCMToken}>
+        <Text style={{color:'white'}}>getFCMToken</Text>
+      </TouchableOpacity>
+  
+      <TouchableOpacity style={{borderRadius:5,backgroundColor:'gray',height:30,width:100,margin:10,justifyContent:'center',alignItems:'center'}}  onPress={handleLogout}>
+        <Text style={{color:'white'}}>logout</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{borderRadius:5,backgroundColor:'gray',height:30,width:100,margin:10,justifyContent:'center',alignItems:'center'}}  onPress={()=>navigation.navigate('Screen1')}>
+        <Text style={{color:'white'}}>Go to Screen1</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 export default HomeScreen
